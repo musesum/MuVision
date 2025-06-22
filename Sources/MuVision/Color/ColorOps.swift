c49a32b7e86b548781e63c45d36240eb3e208ba1
musesum <musesum@gmail.com>
2025-06-21 18:32:50 -0700

    menu flipNW menu sync

--- Sources/MuVision/Color/ColorOps.swift
+++ Sources/MuVision/Color/ColorOps.swift
@@ -7,10 +7,10 @@ public struct ColorOps: OptionSet {
     public let rawValue: Int
 
     public static let gradient = ColorOps(rawValue: 1 << 0) // smooth gradient between left and right
-    public static let black = ColorOps(rawValue: 1 << 1)    // left and right are black
-    public static let white = ColorOps(rawValue: 1 << 2)    // left and right are white
-    public static let zeno = ColorOps(rawValue: 1 << 3)     // zeno fractalize 1/2 + 1/4 + 1/8 ...
-    public static let flip = ColorOps(rawValue: 1 << 4)     // flip right to left
+    public static let black    = ColorOps(rawValue: 1 << 1) // left and right are black
+    public static let white    = ColorOps(rawValue: 1 << 2) // left and right are white
+    public static let zeno     = ColorOps(rawValue: 1 << 3) // zeno fractalize 1/2 + 1/4 + 1/8 ...
+    public static let flip     = ColorOps(rawValue: 1 << 4) // flip right to left
 
     var gradient : Bool { contains(. gradient) }
     var black    : Bool { contains(. black   ) }
